1accc11b30056a0bd714cff538299be209a29d18
anuka <123344+Anubisss@users.noreply.github.com>
2025-03-13 15:20:36 +0100

    Rename a variable

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,23 +1,23 @@
 import { useGoogleLogin } from '@react-oauth/google';
 import { useState } from 'react';
 
-const LOCAL_STORAGE_KEY = 'googleAccessToken';
+const STORAGE_KEY = 'googleAccessToken';
 
 const useLogin = () => {
   const [accessToken, setAccessToken] = useState<string | null>(() => {
-    return sessionStorage.getItem(LOCAL_STORAGE_KEY);
+    return sessionStorage.getItem(STORAGE_KEY);
   });
 
   const login = useGoogleLogin({
     onSuccess: (tokenResponse) => {
-      sessionStorage.setItem(LOCAL_STORAGE_KEY, tokenResponse.access_token);
+      sessionStorage.setItem(STORAGE_KEY, tokenResponse.access_token);
       setAccessToken(tokenResponse.access_token);
     },
     scope: 'https://www.googleapis.com/auth/spreadsheets',
   });
 
   const clearAccessToken = () => {
-    sessionStorage.removeItem(LOCAL_STORAGE_KEY);
+    sessionStorage.removeItem(STORAGE_KEY);
     setAccessToken(null);
   };
 
